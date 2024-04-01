04f48fbb6fc23b29435a83b7d46b0015254e5a43
ninetyrpm <164790025+ninetyrpm@users.noreply.github.com>
2024-03-31 23:25:20 -0400

    Update leaflet-imagekit.js

--- js/leaflet-imagekit.js
+++ js/leaflet-imagekit.js
@@ -3,6 +3,7 @@ async function getSignedImageUrl(imagePath) {
   const endpoint = '/api/imagekit.js';
 
   try {
+    console.log("YOU GOT THIS FAR #1");
     const response = await fetch(endpoint, {
       method: 'POST',
       body: imagePath,
@@ -10,13 +11,16 @@ async function getSignedImageUrl(imagePath) {
     });
 
     if (!response.ok) {
+      console.log("YOU GOT THIS FAR #2");
       throw new Error(`Failed to fetch signed URL: ${response.statusText}`);
+
     }
 
     const data = await response.json();
     return data.imageURL;
   } catch (error) {
     console.error('Error fetching signed URL:', error);
+    console.log("YOU ENDED UP HERE :(")
     // Handle error gracefully in your application (e.g., display an error message)
     return null; // Or throw an error for further handling
   }
