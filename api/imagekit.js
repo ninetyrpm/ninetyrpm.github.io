7f926f1b58375098ed1bfa6738b9601c9c702fb9
ninetyrpm <164790025+ninetyrpm@users.noreply.github.com>
2024-03-31 22:29:58 -0400

    Update imagekit.js

--- api/imagekit.js
+++ api/imagekit.js
@@ -4,6 +4,7 @@ export const runtime = 'nodejs';
 
 const publicKey = process.env.IMAGEKIT_PUBLIC_KEY;
 const privateKey = process.env.IMAGEKIT_PRIVATE_KEY;
+console.log(publicKey, privateKey);
 
 imagekit.config({
   publicKey: publicKey,
