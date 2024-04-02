11abf412b04fc7f1409496b09cf5359f686b0bde
ninetyrpm <164790025+ninetyrpm@users.noreply.github.com>
2024-04-02 00:33:35 -0400

    idkwtfiad

--- api/vercel-imagekit.js
+++ api/vercel-imagekit.js
@@ -8,38 +8,38 @@ if (!process.env.IMAGEKIT_PUBLIC_KEY || !process.env.IMAGEKIT_PRIVATE_KEY) {
   return { error: "Failed to retrieve ImageKit credentials" };
 }
 
-// var imagekit = new ImageKit({
-//   publicKey : publicKey,
-//   privateKey : privateKey,
-//   urlEndpoint : "https://ik.imagekit.io/bcbbiketag/",
-//   // authenticationEndpoint: "/api/vercel-imagekit.js"
-// });
+var imagekit = new ImageKit({
+  publicKey : publicKey,
+  privateKey : privateKey,
+  urlEndpoint : "https://ik.imagekit.io/bcbbiketag/",
+  // authenticationEndpoint: "/api/vercel-imagekit.js"
+});
 
-// console.log("POINT A");
+console.log("POINT A");
 
-// // Default Export
-// async function handler(req, res) {
-//   const { imagePath } = req.body;
+// Default Export
+async function handler(req, res) {
+  const { imagePath } = req.body;
 
-//   try {
-//     var imageURL = imagekit.url({
-//       path: "/tag-photos/Waves.jpg",
-//       queryParameters : {
-//         "v" : "123"
-//       },
-//       transformation : [{
-//         height: 300,
-//         width: 400
-//       }],
-//       signed : true,
-//       expireSeconds : 300
-//     });
-//     console.log("POINT C");
-//     res.status(200).json({ imageURL });
+  try {
+    var imageURL = imagekit.url({
+      path: "/tag-photos/Waves.jpg",
+      queryParameters : {
+        "v" : "123"
+      },
+      transformation : [{
+        height: 300,
+        width: 400
+      }],
+      signed : true,
+      expireSeconds : 300
+    });
+    console.log("POINT C");
+    res.status(200).json({ imageURL });
 
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Failed to generate signed URL" });
-//     console.log("POINT FAIL");
-//   }
-// };
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to generate signed URL" });
+    console.log("POINT FAIL");
+  }
+};
