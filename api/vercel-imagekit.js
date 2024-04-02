96412502bb8218f4c232ee1d9181bd6775384390
ninetyrpm <164790025+ninetyrpm@users.noreply.github.com>
2024-04-02 00:28:05 -0400

    Update vercel-imagekit.js

--- api/vercel-imagekit.js
+++ api/vercel-imagekit.js
@@ -17,29 +17,29 @@ var imagekit = new ImageKit({
 
 console.log("POINT A");
 
-// Default Export
-async function handler(req, res) {
-  const { imagePath } = req.body;
+// // Default Export
+// async function handler(req, res) {
+//   const { imagePath } = req.body;
 
-  try {
-    var imageURL = imagekit.url({
-      path: "/tag-photos/Waves.jpg",
-      queryParameters : {
-        "v" : "123"
-      },
-      transformation : [{
-        height: 300,
-        width: 400
-      }],
-      signed : true,
-      expireSeconds : 300
-    });
-    console.log("POINT C");
-    res.status(200).json({ imageURL });
+//   try {
+//     var imageURL = imagekit.url({
+//       path: "/tag-photos/Waves.jpg",
+//       queryParameters : {
+//         "v" : "123"
+//       },
+//       transformation : [{
+//         height: 300,
+//         width: 400
+//       }],
+//       signed : true,
+//       expireSeconds : 300
+//     });
+//     console.log("POINT C");
+//     res.status(200).json({ imageURL });
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to generate signed URL" });
-    console.log("POINT FAIL");
-  }
-};
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to generate signed URL" });
+//     console.log("POINT FAIL");
+//   }
+// };
